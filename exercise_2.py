b9488131541094cf4823f9464d708236639eb9b2
bthakr1 <b.thakr@mail.analytics.hbs.edu>
2021-02-11 23:50:07 -0600

    changed code

--- exercise_2.py
+++ exercise_2.py
@@ -11,8 +11,12 @@ def last_error_time(df):
     for _ , row in df.iterrows():
         if row['status_code'] < 400:
             continue
-        if not last_time or row['time'] > last_time:
+        if not last_time or row['time'] > last_time:ß
             last_time = row['time']
     return last_time
 
-    
+def load_df(db_file):
+    """Load DataFrame from database"""
+    conn = sqlite3.connect(db_file,detect_types=sqlite3.PARSE_DECLTYPES)
+    with closing(conn):
+        return pd.read_sql('SELECT * FROM logs', conn)
