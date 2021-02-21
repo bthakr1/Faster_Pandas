a83d1ed7ce9c756d668c7057f0fc467bf92672b7
bthakr1 <b.thakr@mail.analytics.hbs.edu>
2021-02-21 11:23:39 -0600

    added some more files

---
+++ running_sum.py
@@ -0,0 +1,19 @@
+
+candies = [2,3,5,1,3]
+
+extraCandies = 3
+
+def kidsWithCandies(candies,extraCandies):
+    maxCandies = max(candies)
+    result = []
+    for i in range(len(candies)):
+        if candies[i] + extraCandies >= maxCandies:
+            result.append(True)
+        else:
+            result.append(False)
+    return result
+
+def inplaceKidsWithCandies(candies,extraCandies):
+    maxCandies = max(candies)
+    return [candy + extraCandies for candy in candies]
+
---
+++ shuffleTheArray.py
@@ -0,0 +1,17 @@
+nums = [2,5,1,3,4,7]
+
+n = 3
+
+
+def shuffle(nums,n):
+    ans = []
+
+    for i in range(n):
+        ans.append(nums[i])
+        ans.append(nums[i+n])
+    return ans
+
+def inPlaceShuffle(nums,n):
+    return [j for i in zip(nums[:n], nums[n:]) for j in i ]
+
+print(inPlaceShuffle(nums,n))
