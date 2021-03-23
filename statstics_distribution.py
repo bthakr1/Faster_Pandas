26bb4e64645ac5f72ad7ac932ad448ba7ab19fd0
bthakr1 <b.thakr@mail.analytics.hbs.edu>
2021-03-23 11:03:08 -0500

    added Central Limit Theorem

--- statstics_distribution.py
+++ statstics_distribution.py
@@ -1,3 +1,6 @@
+# Reference Article 
+# https://towardsdatascience.com/sampling-distributions-with-python-implementation-3f4555e1d450
+
 # Sampling Distributions
 
 # Important in inferential statistics where the goal is to draw conclusion about a population
@@ -61,13 +64,13 @@ import matplotlib.pyplot as plt
 
 # Histogram of both experiments
 
-fig = plt.figure(figsize=(10,7))
-bins = np.linspace(0,1,10)
-plt.hist(sample_props,bins=bins,label="Sample Props with 5 sample size")
-plt.hist(sample_props_20,bins=bins,label="Sample Props with 20 sample size")
-plt.title("Histogram with sample size of 5 vs 20")
-plt.legend(loc='upper left')
-#plt.show()
+# fig = plt.figure(figsize=(10,7))
+# bins = np.linspace(0,1,10)
+# plt.hist(sample_props,bins=bins,label="Sample Props with 5 sample size")
+# plt.hist(sample_props_20,bins=bins,label="Sample Props with 20 sample size")
+# plt.title("Histogram with sample size of 5 vs 20")
+# plt.legend(loc='upper left')
+# #plt.show()
 
 
 # Law of large number
@@ -99,5 +102,45 @@ print("Mean with 10,000 samples : ", np.random.choice(puppies,size=10_000, repla
 # Central Limit Theorem
 
 # With a large enough sample size, the sampling distribution of the the mean will be 
-# normally distributed
+# normally distributed.
+
+# Applies to following scneario.
+
+# Sample Means
+# Difference between sample means
+# Sample Proportions
+# Difference between sample proportions
+
+# Gamms distribution arises when the waiting time or any quantity (in denominator) are relevant to each other.
+# Compared to Poisson , where events or rate are not related to each other. 
+# Gamms is used in queuing model, climatology, and finance. 
+# Amount of rainfall accumulated in reservoir
+# Load of web servers
+
+puppies_data = np.random.gamma(1,100,size=1000)
+
+
+# Now lets create 4 bins with different number of samples taken from the above gamma distribution
+
+mean_size_3 = []
+mean_size_10 = []
+mean_size_20 = []
+mean_size_30 = []
+
+for i in range(10_000):
+    mean_size_3.append(np.random.choice(puppies_data,size=(1,3),replace=True).mean())
+    mean_size_10.append(np.random.choice(puppies_data,size=(1,10),replace=True).mean())
+    mean_size_20.append(np.random.choice(puppies_data,size=(1,20),replace=True).mean())
+    mean_size_30.append(np.random.choice(puppies_data,size=(1,30),replace=True).mean())
+
+
+plt.figure(figsize=(7,10))
+plt.hist(mean_size_3,label="Mean with sample size of 3")
+plt.hist(mean_size_10,label="Mean with sample size of 10")
+plt.hist(mean_size_20,label="Mean with sample size of 20")
+plt.hist(mean_size_30,label="Mean with sample size of 30")
+plt.axvline(puppies_data.mean(),color='k',linestyle='dashed',linewidth=1)
+plt.legend(loc='upper right')
+plt.show()
+
 
