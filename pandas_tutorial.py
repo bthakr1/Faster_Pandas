3d4472c53bedd20c620cee82f802ee394b4c283d
bthakr1 <b.thakr@mail.analytics.hbs.edu>
2021-04-04 12:08:58 -0500

    added iloc and loc methods

--- pandas_tutorial.py
+++ pandas_tutorial.py
@@ -88,3 +88,80 @@ print("Only Date Columns : ", only_date)
 except_object = df.select_dtypes(exclude='object').columns
 
 print("Everything Except Object Columns : ", except_object)
+
+# convert strings to numbers
+
+df = pd.DataFrame({'col_one':['1.1','2.2','3.3'],
+                    'col_two': ['4.4','5.5','6.6'],
+                    'col_three': ['7.7','8.8','-']
+
+})
+
+print("Dataframe without type conversion")
+print(df)
+
+df.astype({'col_one':'float','col_two':'float'}).dtypes
+print("After changing one and two column")
+print(df)
+
+pd.to_numeric(df.col_three,errors='coerce')
+print("Using Coerce to change the third column")
+print(df)
+
+df = df.apply(pd.to_numeric,errors='coerce').fillna(0)
+print("Changing all three at once using Apply")
+print(df)
+
+# Reduce data frame size
+
+print(sns.get_dataset_names())
+
+df = sns.load_dataset('car_crashes')
+
+print(df.info())
+
+print(df.info(memory_usage='deep'))
+
+print(df.head())
+
+# print based on location
+
+# We will use index location for this
+# Here we are printing 3 to 8 rows and only one column i.e. 1
+print("\n")
+print("Printing Rows from 3 to 8 and Columns from 1 to 2")
+print("\n")
+print(df.iloc[3:9,1:3])
+
+
+# Now let's use the loc parameter
+
+print("\n")
+df.set_index('abbrev',inplace=True)
+
+# Printing the index for AR, AZ , and OK with columns from "alcohol" to "ins_premuim"
+
+print(df.loc[['AR','AZ','OK'],'alcohol':'ins_premium'])
+
+# Let's select some other csv and try iloc and loc with that dataframe
+
+df = pd.read_csv('https://s3-eu-west-1.amazonaws.com/shanebucket/downloads/uk-500.csv')
+
+# Let's print the first two columns and 7-12 rows
+
+print("Let's print the first two columns and 7-12 rows")
+print(df.iloc[7:13,1:3])
+
+# Let's print all columns and 15-35 rows
+
+print(df.iloc[15:36,:])
+
+# How about 3 to 7 column and 19-22 rows
+
+print(df.iloc[19:23,3:8])
+
+# Next we will try loc
+
+df.set_index('city',inplace=True)
+
+print(df.head())
